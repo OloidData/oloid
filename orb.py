8a2038e056a2761303e60a2fed73eaefb1e28634
SioKCronin <siobhankcronin@gmail.com>
2018-04-16 16:05:37 -0700

    WIP

--- orb.py
+++ orb.py
@@ -1,12 +1,18 @@
 import requests
 import time
-import numpy as np
+import math
 
-for i in range(3):
-    x = requests.get('http://192.168.1.14/api/YwWVvDWK9xetVtmvTPRSnaGBBPfjRwjc3nBvLZTy/lights/5')
-    #print(x.text)
-    r = requests.put('http://192.168.1.14/api/YwWVvDWK9xetVtmvTPRSnaGBBPfjRwjc3nBvLZTy/lights/5/state',
-            data = {'bri':'{}'.format(np.random.randint(0,50)), 'transitiontime':20})
-    print(r.text)
-    time.sleep(1)
+#x = requests.get('http://192.168.1.14/api/YwWVvDWK9xetVtmvTPRSnaGBBPfjRwjc3nBvLZTy/lights/5')
+#print(x.text)
+
+while True:
+    for i in range(30):
+        value = i/30 * math.pi
+        print("value", value)
+        brightness = math.floor(math.sin(value) * 250) + 4
+        print("brightness", brightness)
+        r = requests.put('http://192.168.1.14/api/YwWVvDWK9xetVtmvTPRSnaGBBPfjRwjc3nBvLZTy/lights/5/state',
+                json = {'bri':brightness, 'transitiontime':3})
+        time.sleep(0.3)
+    time.sleep(0.35)
 
